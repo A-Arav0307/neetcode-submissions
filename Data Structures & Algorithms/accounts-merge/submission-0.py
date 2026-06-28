9c172da74ab57bea05e5613ed251781b671de213
A-Arav0307 <143667922+A-Arav0307@users.noreply.github.com>
2026-06-28 11:07:01 -0500

    Add: accounts-merge - submission-0

---
+++ Data Structures & Algorithms/accounts-merge/submission-0.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        self.parent = {}
+        self.email_to_name = {}
+
+        def find(x): 
+            while x != self.parent[x]: 
+                self.parent[x] = self.parent[self.parent[x]]
+                x = self.parent[x]
+            return x
+
+        def union(n1, n2):
+            root_a = find(n1)
+            root_b = find(n2)
+
+            if root_a != root_b: 
+                self.parent[root_a] = root_b 
+
+        for account in accounts:
+            name = account[0]
+
+            for email in account[1:]:
+                self.parent[email] = email
+                self.email_to_name[email] = name
+
+        groups = defaultdict(list)
+
+        for account in accounts: 
+            first_email = account[1]
+            for email in account[2:]:
+                union(first_email, email)
+
+        for email in self.parent:
+            root = find(email)
+            groups[root].append(email)
+
+        for email in groups:
+            name = self.email_to_name[email]
+            groups[email].insert(0, name) 
+
+        return list(groups.values())
+
+        
