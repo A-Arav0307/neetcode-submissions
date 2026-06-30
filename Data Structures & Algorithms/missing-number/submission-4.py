db320e2cc768c46de4d945b0b23855cc2341f6a1
A-Arav0307 <143667922+A-Arav0307@users.noreply.github.com>
2026-06-30 11:06:42 -0500

    Add: missing-number - submission-4

---
+++ Data Structures & Algorithms/missing-number/submission-4.py
@@ -0,0 +1,8 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums) 
+        nums.sort()
+        for num in range(n):
+            if num ^ nums[num] != 0:
+                return num
+        return num+1
